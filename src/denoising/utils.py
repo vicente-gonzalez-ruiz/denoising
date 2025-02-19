a692a78261a017200bc7f49c2311182034739c2a
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2025-02-19 05:24:22 +0100

    .

--- src/denoising/utils.py
+++ src/denoising/utils.py
@@ -2,15 +2,16 @@ import numpy as np
 import scipy.ndimage
 
 def get_gaussian_kernel(sigma=1):
+    """Generates a 1D Gaussian kernel using scipy.ndimage.gaussian_filter1d()."""
     number_of_coeffs = 3
     number_of_zeros = 0
     while number_of_zeros < 2 :
         delta = np.zeros(number_of_coeffs)
-        delta[delta.size//2] = 1
+        delta[delta.size//2] = 1 # Impulse at center
         coeffs = scipy.ndimage.gaussian_filter1d(delta, sigma=sigma)
         number_of_zeros = coeffs.size - np.count_nonzero(coeffs)
         number_of_coeffs += 1
-    return coeffs[1:-1]
+    return coeffs[1:-1]  # Remove the first and last zero elements
 
 def gaussian_noise(shape, mean=0, sigma=20):
     noise = np.random.normal(mean, sigma, shape).reshape(shape)
